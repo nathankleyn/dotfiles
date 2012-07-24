8d1d8e78618453e37d9f9a037ba937333fdc5022
Nathan Kleyn <nathan@unfinitydesign.com>
2012-07-24 14:42:34 +0100

    Annotating Xmonad config source and updating keybinding for volume to actually freaking work!

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,40 +6,71 @@ import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.EZConfig
 import XMonad.Util.Loggers
 
+---
 -- The main function.
-main = xmonad =<< (statusBar barCommand barPrettyPrint toggleStrutsKey myConfig)
+main = xmonad =<< xmobarFn
 
--- The xmobar colours
-highlightColour = "#FF9"
-hiddenColour = "#CCC"
-disabledColour = "#888"
-backgroundColour = "#000"
+---
+-- Xmobar
+-- This is the toolbar that sits at the top of the main X screen.
+xmobarFn = (statusBar "xmobar" xmobarPretty xmobarToggleKey xmonadConfig)
 
-barCommand = "xmobar"
-barPrettyPrint = xmobarPP {
-        ppTitle = xmobarColor highlightColour "" . shorten 100,
-        ppCurrent = xmobarColor highlightColour "",
-        ppVisible = xmobarColor hiddenColour "",
-        ppHidden = xmobarColor hiddenColour "",
-        ppHiddenNoWindows = xmobarColor disabledColour ""
+xmobarHighlight = "#FF9"
+xmobarHidden = "#CCC"
+xmobarDisabled = "#888"
+
+-- These are options that are passed to Xmobar when it's started up. We define
+-- nice colours here.
+xmobarPretty = xmobarPP {
+        ppTitle = xmobarColor xmobarHighlight "" . shorten 100,
+        ppCurrent = xmobarColor xmobarHighlight "",
+        ppVisible = xmobarColor xmobarHidden "",
+        ppHidden = xmobarColor xmobarHidden "",
+        ppHiddenNoWindows = xmobarColor xmobarDisabled ""
        }
 
-toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
+-- This is the key that can be used to toggle the visibility of the Xmobar
+-- at the top of the screen. In this case, it's <Mod>+b
+xmobarToggleKey XConfig {
+                  XMonad.modMask = modMask
+                } = (modMask, xK_b)
 
-myConfig = gnomeConfig {
-              modMask = mod4Mask,
-              normalBorderColor = "#FFF",
-              focusedBorderColor = "#000",
-              borderWidth = 1,
-              handleEventHook = fullscreenEventHook,
-              terminal = "terminator"
-            }
-            `additionalKeysP`
-             [
-               -- XF86AudioMute
-                 ("<XF86AudioMute>", spawn "amixer -q set PCM toggle")
-               -- XF86AudioLowerVolume
-               , ("<XF86AudioLowerVolume>", spawn "amixer -q set PCM 1+ unmute")
-               -- XF86AudioRaiseVolume
-               , ("<XF86AudioLowerVolume>", spawn "amixer -q set PCM 1- unmute")
-             ]
+---
+-- The XMonad configuration.
+-- This is an object setup using record syntax that contains a boat load of
+-- things you can customise. Each of them are detailed below.
+gConfig = gnomeConfig {
+            -- Use the Windows key as the modifier for shortcuts.
+            modMask = mod4Mask,
+            -- Set the border colour for unfocused windows.
+            normalBorderColor = "#FFF",
+            -- And the border colour for focused windos.
+            focusedBorderColor = "#000",
+            -- The width of the borders around windows.
+            borderWidth = 1,
+            -- When an event takes place, we can define a callback to be
+            -- triggered on receipt of this. In our case, we want to handle
+            -- full screening things like Chromium and Flash properly.
+            handleEventHook = fullscreenEventHook,
+            -- Set the terminal that is opened when the <Mod>+<Enter> shortcut
+            -- is pressed.
+            terminal = "gnome-terminal"
+          }
+
+xmonadKeyMap = [
+           -- Mute key
+           -- Either the correct Fn key, or <Mod>+<Backspace>
+             ("<XF86AudioMute>", spawn "amixer -q set Master toggle")
+           , ("M-<Backspace>", spawn "amixer -q set Master toggle")
+           -- Volume up key
+           -- Either the correct Fn key, or <Mod>+<Up>
+           , ("<XF86AudioRaiseVolume>", spawn "amixer -q set Master 1%+ unmute")
+           , ("M-<Up>", spawn "amixer -q set Master 1%+ unmute")
+           -- Volume lower key
+           -- Either the correct Fn key, or <Mod>+<Down>
+           , ("<XF86AudioLowerVolume>", spawn "amixer -q set Master 1%- unmute")
+           , ("M-<Down>", spawn "amixer -q set Master 1%- unmute")
+         ]
+
+-- Define the configuration.
+xmonadConfig = gConfig `additionalKeysP` xmonadKeyMap
