e544040844b9d1aed031565a0a90c4078443ef6c
Nathan Kleyn <nathan@nathankleyn.com>
2018-03-09 12:40:55 +0000

    Cleanup aliases in Fish config, latest IntelliJ/Atom settings/packages.

--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,6 @@ source ~/.fish-private
 
 # Add directories to the PATH
 
-# set -x PATH $PATH ~/.bin
-# set -x PATH $PATH ~/.npm/bin
-# set -x PATH $PATH ~/.yarn/bin
-# set -x PATH $PATH /opt/hbase/bin
 set -x PATH $PATH /usr/local/bin
 set -x PATH $PATH /usr/local/opt/ruby/bin
 set -x PATH $PATH /usr/local/sbin
@@ -18,26 +14,16 @@ set -x PATH $PATH ~/.okta-aws-cli
 set -x _JAVA_OPTIONS "-Djava.net.preferIPv4Stack=true -Djava.security.krb5.realm=OX.AC.UK -Djava.security.krb5.kdc=kdc0.ox.ac.uk:kdc1.ox.ac.uk"
 set -x BROWSER chromium
 set -x CHROME_BIN /Users/nathan/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome
-set -x DOCKER_API_VERSION 1.23
 set -x DOCKER_HOST unix:///var/run/docker.sock
 set -x EDITOR vim
 set -x FIREFOX_BIN /Users/nathan/Applications/Firefox.app/Contents/MacOS/firefox
 set -x FLEETCTL_ENDPOINT http://localhost:4001
 set -x GEM_HOME ~/.gem
 set -x HOMEBREW_NO_ANALYTICS 1
-set -x IHQ_HOME ~/repos/ihq
 set -x LANG en_US.UTF-8
 set -x NIC_CAGE_SUCKS 1
 
 # Make some functions to do handy things.
-
-function ihq
-  cd $IHQ_HOME
-end
-
-function ihqp
-  ihq; and cd platform
-end
 
 function gs
   git status $argv
@@ -45,10 +31,6 @@ end
 
 function gc
   git clean -dfx -e .vagrant -e mesos $argv
-end
-
-function gf
-  git commit --fixup HEAD; and git fetch; and env EDITOR=true git rebase --autosquash -i origin/master
 end
 
 function ll
@@ -59,14 +41,11 @@ function rspec
   rspec -c -b $argv
 end
 
-function thor
-  bundle exec thor $argv
-end
-
 function typora
   open -a Typora $argv
 end
 
-eval (direnv hook fish)
+# Extend Fish with direnv and iTerm integrations.
 
+eval (direnv hook fish)
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
