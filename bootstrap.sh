184722ab60bad2039800dc44788e9aff98d0149b
Nathan Kleyn <nathan@nathankleyn.com>
2018-08-06 11:09:49 +0100

    Fix fzf installation in Fish shell.

--- bootstrap.sh
+++ bootstrap.sh
@@ -13,10 +13,10 @@ brew tap homebrew/cask
 brew tap homebrew/cask-fonts
 brew tap homebrew/cask-versions
 brew tap fisherman/tap
-brew install direnv fish fisherman graphviz thrift ruby rustup direnv
-brew cask install atom docker font-hasklig font-source-code-pro font-source-code-pro-for-powerline gpg-suite insomnia iterm2 java8 jetbrains-toolbox typora vlc visual-studio-code visualvm zerotier-one
+brew install --force direnv fish fisherman fzf graphviz thrift ruby rustup direnv
+brew cask install --force atom docker font-hasklig font-source-code-pro font-source-code-pro-for-powerline gpg-suite insomnia iterm2 java8 jetbrains-toolbox typora vlc visual-studio-code visualvm zerotier-one
 # This must be run after Java is installed.
-brew install scala sbt
+brew install --force scala sbt
 
 gem install bundler
 rustup-init -y
@@ -24,7 +24,7 @@ rustup install beta
 rustup install nightly
 
 curl -L https://get.oh-my.fish | fish
-fish -c "fisher install edc/bass fzf imgcat ; and __fzf_install"
+fish -c "fisher install edc/bass fzf imgcat"
 fish -c "omf install bobthefish"
 
 fish -c "cd $DOTFILES_ROOT; and rake"
