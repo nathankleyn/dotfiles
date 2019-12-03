c3e6afba41f91abd3548ac9cc2f6c7053a4075bb
Nathan Kleyn <nathan@nathankleyn.com>
2019-12-03 08:19:44 +0000

    Switch to using `brew bundle` to install all Brew stuff.
    For all brew taps, packages and casks, install using a Brew bundle which
    automates the entire process.

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,19 +8,12 @@ cd "$DOTFILES_ROOT"
 xcode-select --install || true
 
 if [ "$(command -v brew)" = "brew not found" ]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew tap homebrew/cask
-brew tap homebrew/cask-fonts
-brew tap AdoptOpenJDK/openjdk
+brew bundle
 
 curl https://git.io/fisher --create-dirs -sLo ~/.config/fish/functions/fisher.fish
-
-brew install --force direnv fish fzf graphviz thrift ruby rustup watch
-brew cask install --force adoptopenjdk8 docker font-source-code-pro font-source-code-pro-for-powerline gpg-suite insomnia iterm2 jetbrains-toolbox shellcheck typora vlc visual-studio-code visualvm zerotier-one
-# This must be run after Java is installed.
-brew install --force scala sbt
 
 GEM_HOME=~/.gem gem install bundler
 
