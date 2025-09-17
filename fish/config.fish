124b7ceca86ce679e7dceae8904d4096cebeb87d
Nathan Kleyn <nathan@nathankleyn.com>
2025-09-17 08:21:44 +0100

    Update various colours and extensions from the last few months

--- fish/config.fish
+++ fish/config.fish
@@ -54,12 +54,10 @@ end
 direnv hook fish | source
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
-# Enable Rbenv
-. (rbenv init - | source)
-
 # Enable fnm
-# fnm env --use-on-cd | source
+fnm env --use-on-cd | source
 
-# Enable Google Cloud auto completions
-source "$(brew --prefix)/share/google-cloud-sdk/path.fish.inc"
 
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
